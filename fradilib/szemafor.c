3e9d47dd856b95d2035b81ebd2d3fb6773df2e35
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2021-05-09 20:15:50 +0200

    semaphore starting value removed, default is 0

--- fradilib/szemafor.c
+++ fradilib/szemafor.c
@@ -1,26 +1,20 @@
-#include <stdlib.h>     // exit
-#include <stdio.h>      // perror
-#include <semaphore.h>  // szemaforkezelés
-#include <fcntl.h>      // szemaforkreálás opciók
+#include <fcntl.h>     // szemaforkreálás opciók
+#include <semaphore.h> // szemaforkezelés
+#include <stdio.h>     // perror
+#include <stdlib.h>    // exit
 
 #include "szemafor.h"
 
-sem_t* szemafor_letrehozas(char*nev,int szemafor_ertek){
-    sem_t *semid=sem_open(nev,O_CREAT,S_IRUSR|S_IWUSR,szemafor_ertek );
-	if (semid==SEM_FAILED){
+sem_t *szemafor_letrehozas(char *nev) {
+    sem_t *semid = sem_open(nev, O_CREAT, S_IRUSR | S_IWUSR, 0);
+    if (semid == SEM_FAILED) {
         perror("sem_open");
-    }  
+    }
     return semid;
 }
 
-void szemafor_mehet(sem_t* szemafor){
-    sem_post(szemafor);
-}
+void szemafor_mehet(sem_t *szemafor) { sem_post(szemafor); }
 
-void szemafor_var(sem_t* szemafor){
-    sem_wait(szemafor);
-}
+void szemafor_var(sem_t *szemafor) { sem_wait(szemafor); }
 
-void szemafor_torles(char* nev){
-    sem_unlink(nev);
-}
+void szemafor_torles(char *nev) { sem_unlink(nev); }
