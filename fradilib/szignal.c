c64cd0998881f7deb04efc22b38c102b589f88e9
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2021-05-09 20:10:36 +0200

    signal handler (int) corrected

--- fradilib/szignal.c
+++ fradilib/szignal.c
@@ -1,31 +1,28 @@
-#include <stdio.h>      // printf
-#include <unistd.h>     // getpid
-#include <signal.h>     // SIGRTMIN
+#include <signal.h> // SIGRTMIN
+#include <stdio.h>  // printf
+#include <unistd.h> // getpid
 
 #include "szignal.h"
 
-// Ez a függvény a leginkábbb "haszontalan" a gyűjteményben, de volt már problémám a paraméterek sorrendjének elfelejtésével, a dokumentációs komment remélhetőleg segít.
-int szignalkuldes(pid_t kinek, int mit){
-    return kill(kinek, mit);
-}
+// Ez a függvény a leginkábbb "haszontalan" a gyűjteményben, de volt már problémám a paraméterek
+// sorrendjének elfelejtésével, a dokumentációs komment remélhetőleg segít.
+int szignalkuldes(pid_t kinek, int mit) { return kill(kinek, mit); }
 
-void handler(int sign){
-    printf("%d folyamat %d jelzést kapott.\n",getpid(), sign);
-}
+void handler(int sign) { printf("%d folyamat %d jelzést kapott.\n", getpid(), sign); }
 
-int numberSignal(int signal){
+void numberSignal(int signal) {
     signal = signal - SIGRTMIN;
     printf("%d folyamat jelzésen keresztül ezt a számot kapta: %d", getpid(), signal);
     return signal;
 }
 
-void numOverSignal(int signal){
+void numOverSignal(int signal) {
     signal = signal - SIGRTMIN;
-    if(signal == 0){
+    if (signal == 0) {
         // SIGRTMIN jelentése: nincs készleten a könyv
-        printf("%d: Kár, hogy nincs.\n",getpid());
-    }else{
+        printf("%d: Kár, hogy nincs.\n", getpid());
+    } else {
         // SIGRTMIN+1 jelentése: készleten van a könyv
-        printf("%d: Köszönöm, kérek egyet!\n",getpid()); 
+        printf("%d: Köszönöm, kérek egyet!\n", getpid());
     }
 }
--- fradilib/szignal.h
+++ fradilib/szignal.h
@@ -7,24 +7,24 @@
  * @param kinek A jelzés címzettjének process ID-je
  * @param mit Az elküldendő jelzés
  * @returns 0, ha sikeres volt a jelzésküldés, -1, ha hiba történt
-*/
+ */
 int szignalkuldes(pid_t kinek, int mit);
 
 /**
  * Egyszerű signal handler függvény
  * @returns A konzolon kijelzi, hogy melyik folyamat hányas signalt kapott.
-*/
+ */
 void handler(int sign);
 
 /**
  * Szám fogadása signalon keresztül. Az érkező számot kiírja a konzolra, valamint
  * @returns a signalon érkező szám
-*/
-int numberSignal(int signal);
+ */
+void numberSignal(int signal);
 
 /**
  * Egyszerű példa a signalokkal való információ-közvetítésre.
-*/
+ */
 void numOverSignal(int signal);
 
 #endif
